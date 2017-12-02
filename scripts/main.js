c39fc03dfe557e17e07add85343821b713ba5420
jsksjs <jsksjs12@live.com>
2017-12-01 19:55:29 -0500

    Initial commit. Basic functionality.

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+// called on input change
+function format(){
+    if(document.getElementById("input").value.length > 0)
+        document.getElementById("output").textContent = scrubText();
+    else
+        document.getElementById("output").textContent = "";
+}
+
+// copy output with button press
+function copyOut(){
+    document.getElementById("output").select();
+    document.execCommand('copy');
+}
+
+// formats MLA standard with proper capitalization and page numbers
+function scrubText()
+{
+    let text = document.getElementById("input").value;
+    let fixed = "\"" + text.replace(/(\r\n|\r|\n)/gm, " ").replace(/"/gm, "'") + "\"";
+    let author = document.getElementById("authorIn").value;
+    let page = document.getElementById("pageNum").value;
+    if(author.length === 0 && page.length === 0){
+        return fixed.replace(/\s+/g, " ");
+    }
+    else{
+        fixed = fixed.replace(/\s+/gm, " ") + " ";
+        let cite = "";
+        if(author.length > 0 && /\S/.test(author)){
+            // create array of names, for each element capitalize first letter and join to rest of name,
+            // then join all names together into string
+            cite = author.split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1)).join(' ');
+            cite = cite.replace(/\sAnd\s/, " and ").replace(/\sEt Al\./, " et al.");
+        }
+        if(page.length > 0 && /\S/.test(page)){
+            if(cite.length > 0)
+                cite += " " + page;
+            else{
+                cite = page;
+            }
+        }
+        fixed += "(" + cite + ")";
+    }
+    return fixed;
+}
